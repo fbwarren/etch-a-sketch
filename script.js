c251306d9592e46993c4d0f12c68045851920e48
fbwarren <fbwarren@berkeley.edu>
2022-12-22 00:45:34 -0800

    color selector

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-// https://stackoverflow.com/questions/5560248/
-function RGB_Log_Shade (p, c) {
-    var i = parseInt, r = Math.round, [a, b, c, d] = c.split(","), P = p < 0, t = P ? 0 : p * 255 ** 2, P = P ? 1 + p : 1 - p;
-    return "rgb" + (d ? "a(" : "(") + r((P * i(a[3] == "a" ? a.slice(5) : a.slice(4)) ** 2 + t) ** 0.5) + "," + r((P * i(b) ** 2 + t) ** 0.5) + "," + r((P * i(c) ** 2 + t) ** 0.5) + (d ? "," + d : ")");
-}
+function drawGrid(v) {
+    let rightColumn = document.querySelector('.right-column');
+    let sliderText = document.querySelector('.text');
 
-function clearScreen() {
-    cells.forEach(cell => cell.style.backgroundColor = 'rgb(255, 255, 255');
-}
+    for (let i = 0; i < v; i++) {
+        let row = document.createElement('div');
+        row.className = 'row';
+        row.style.display = 'flex';
+        row.style.maxWidth = '66vmin';
+        for (let j = 0; j < v; j++) {
+            let cell = document.createElement('div');
+            cell.className = 'cell'
+            cell.id = `${i * v + j}`;
+            cell.style.backgroundColor = 'rgb(255, 255, 255)'
+            cell.style.outline = 'black solid 1px';
+            cell.style.flex = '1';
+            cell.style.aspectRatio = '1/1';
+            row.appendChild(cell);
+        }
+        rightColumn.appendChild(row);
+    }
 
-function toggleGrid() {
+    let cells = document.querySelectorAll('.cell');
     cells.forEach(cell => {
-        cell.style.outlineStyle = (cell.style.outlineStyle == 'solid') ? 'None' : 'solid';
+        cell.addEventListener('mouseover', colorCell);
+        cell.addEventListener('mousedown', colorCell);
     });
+
+    sliderText.appendChild(document.createTextNode(`${v} x ${v}`));
 }
 
-function changeMode(newMode) {
-    mode = newMode;
+function redrawGrid(e) {
+    cells.forEach(cell => cell.remove());
+    let sliderText = document.querySelector('.text')
+    sliderText.removeChild(sliderText.childNodes[0]);
+    drawGrid(e);
+    cells = document.querySelectorAll('.cell');
 }
 
 function colorCell(e) {
@@ -43,45 +62,31 @@ function colorCell(e) {
     }
 }
 
-function redrawGrid(e) {
-    cells.forEach(cell => cell.remove());
-    drawGrid(e);
-    cells = document.querySelectorAll('.cell');
+function clearScreen() {
+    cells.forEach(cell => cell.style.backgroundColor = 'rgb(255, 255, 255');
 }
 
-function drawGrid(v) {
-    let rightColumn = document.querySelector('.right-column');
-    let slider = document.querySelector('.slider');
-
-    for (let i=0; i<v; i++) {
-        let row = document.createElement('div');
-        row.className = 'row';
-        row.style.display = 'flex';
-        row.style.maxWidth = '66vmin';
-        for (let j=0; j<v; j++) {
-            let cell = document.createElement('div');
-            cell.className = 'cell'
-            cell.id = `${i*v + j}`;
-            cell.style.backgroundColor = 'rgb(255, 255, 255)'
-            cell.style.outline = 'black solid 1px';
-            cell.style.flex = '1';
-            cell.style.aspectRatio = '1/1';
-            row.appendChild(cell);
-        }
-        rightColumn.appendChild(row);
-    }
-
-    let cells = document.querySelectorAll('.cell');
+function toggleGrid() {
     cells.forEach(cell => {
-        cell.addEventListener('mouseover', colorCell);
-        cell.addEventListener('mousedown', colorCell);
+        cell.style.outlineStyle = (cell.style.outlineStyle == 'solid') ? 'None' : 'solid';
     });
+}
 
-    slider.appendChild(document.createTextNode(`${v} x ${v}`));
+function changeMode(newMode) {
+    mode = newMode;
+}
+
+function changeColor(newColor) {
+    color = newColor;
+}
+
+// https://stackoverflow.com/questions/5560248/
+function RGB_Log_Shade(p, c) {
+    var i = parseInt, r = Math.round, [a, b, c, d] = c.split(","), P = p < 0, t = P ? 0 : p * 255 ** 2, P = P ? 1 + p : 1 - p;
+    return "rgb" + (d ? "a(" : "(") + r((P * i(a[3] == "a" ? a.slice(5) : a.slice(4)) ** 2 + t) ** 0.5) + "," + r((P * i(b) ** 2 + t) ** 0.5) + "," + r((P * i(c) ** 2 + t) ** 0.5) + (d ? "," + d : ")");
 }
 
 let color = 'rgb(0, 0, 0)';
 let mode = 'regular';
-
 drawGrid(16);
-let cells = document.querySelectorAll('.right-column .row .cell');
+cells = document.querySelectorAll('.cell');
