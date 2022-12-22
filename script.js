c39fe2a9e8a4a33669ad99563006e055eb801705
fbwarren <fbwarren@berkeley.edu>
2022-12-21 23:49:34 -0800

    added different modes

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
 function clearScreen() {
-    cells.forEach(cell => cell.style.backgroundColor = 'white');
+    cells.forEach(cell => cell.style.backgroundColor = '#FFFFFF');
 }
 
 function toggleGrid() {
     cells.forEach(cell => {
-        if (cell.style.outlineStyle == 'solid')
-            cell.style.outlineStyle = 'None';
-        else
-            cell.style.outlineStyle = 'solid';
+        cell.style.outlineStyle = (cell.style.outlineStyle == 'solid') ? 'None' : 'solid';
     });
+}
+
+function changeMode(newMode) {
+    mode = newMode;
 }
 
 function colorCell(e) {
     if (e.buttons == 1 || e.buttons == 3) {
-        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        this.style.backgroundColor = '#' + randomColor;
+        switch (mode) {
+            case 'regular':
+                this.style.backgroundColor = color;
+                break;
+            case 'eraser':
+                this.style.backgroundColor = '#FFFFFF';
+                break;
+            case 'rainbow':
+                let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+                this.style.backgroundColor = '#' + randomColor;
+                break;
+            case 'shading':
+                this.style.backgroundColor = '#FFFFFF';
+                break;
+        }
     }
 }
 
-function newGrid(e) {
+function redrawGrid(e) {
     cells.forEach(cell => cell.remove());
-    generatePixels(e);
+    drawGrid(e);
     cells = document.querySelectorAll('.cell');
 }
 
-function generatePixels(v) {
+function drawGrid(v) {
     let rightColumn = document.querySelector('.right-column');
+    let slider = document.querySelector('.slider');
 
     for (let i=0; i<v; i++) {
         let row = document.createElement('div');
         row.className = 'row';
         row.style.display = 'flex';
-        row.style.maxWidth = '500px';
+        row.style.maxWidth = '66vmin';
         for (let j=0; j<v; j++) {
             let cell = document.createElement('div');
             cell.className = 'cell'
@@ -49,7 +64,11 @@ function generatePixels(v) {
         cell.addEventListener('mouseover', colorCell);
         cell.addEventListener('mousedown', colorCell);
     });
+    slider.textContent = `${v} x ${v}`;
 }
 
-generatePixels(16);
+let color = '#000000';
+let mode = 'regular';
+
+drawGrid(16);
 let cells = document.querySelectorAll('.right-column .row .cell');
